4f1bdc1f8fdea75123d799519837734ce37cb2bb
Diego Soria Rios <diego@Diegos-MacBook-Pro.local>
2023-02-15 10:06:08 +0000

    Fix build routes

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,18 @@ export default defineConfig({
   server: {
     port: 8000,
     hot: true,
-  }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        list: resolve(__dirname, 'src/pages/List/index.html'),
+        form: resolve(__dirname, 'src/pages/Form/index.html'),
+        login: resolve(__dirname, 'src/pages/Login/index.html'),
+        moderation: resolve(__dirname, 'src/pages/moderation/index.html'),
+        profile: resolve(__dirname, 'src/pages/Profile/index.html'),
+        question: resolve(__dirname, 'src/pages/Question/index.html'),
+      },
+    },
+  },
 })
