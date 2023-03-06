e1ecdfd59dd4f73ba39b5cc920bc7f3b8007070d
Diego Soria Rios <diego@Diegos-MacBook-Pro.local>
2023-03-06 15:00:39 +0000

    Fix Variables

---
+++ env_variables.js
@@ -0,0 +1,7 @@
+export const API_KEY=""
+export const AUTH_DOMAIN=""
+export const PROJECT_ID=""
+export const STORAGE_BUCKET=""
+export const MESSAGING_SENDER_ID=""
+export const APP_ID=""
+export const DATABASE_URL=""
